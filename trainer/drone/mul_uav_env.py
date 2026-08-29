0e8e87ead7bea39b478dbc0c2a62556bb9542c66
huskyth <1622313177@qq.com>
2026-08-29 20:50:16 +0800

    f

--- trainer/drone/mul_uav_env.py
+++ trainer/drone/mul_uav_env.py
@@ -665,13 +665,13 @@ class MultiUavEnv:
                     # 连续闪避，计数+1
                     self.dodge_count += 1
                     # 递增奖励：第一次 +0.5，第二次 +0.8，第三次 +1.1...
-                    r_dodge = 0.5 + 0.3 * (self.dodge_count - 1)
+                    r_dodge = 0.005 + 0.003 * (self.dodge_count - 1)
                     r_dodge = min(r_dodge, 2.0)  # 上限 2.0
                     self.r_msg[idx] += f'闪避x{self.dodge_count}+{r_dodge:.2f}, '
                 else:
                     # 开火但没闪避 → 重置计数！
                     self.dodge_count = 0
-                    r_dodge = -0.2
+                    r_dodge = -0.002
                     self.r_msg[idx] += '闪避中断-0.2, '
             else:
                 # 不是开火状态 → 重置计数！
